05df7f9f9278d9fff48e268abfaee524d3dcacf9
Aysaaak <isaac.6severino@gmail.com>
2025-02-28 21:00:57 +0800

    More dummy, some changes in main

--- app/src/main/java/com/example/personal_dictionary_app/MainActivity.java
+++ app/src/main/java/com/example/personal_dictionary_app/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.personal_dictionary_app;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button add;
+    private SearchView search;
+    private Spinner category;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        add = findViewById(R.id.button);
+        search = findViewById(R.id.searchView);
+        category = findViewById(R.id.spinner);
+
+        add.setOnClickListener(view -> {
+
+
         });
     }
 }
--- app/src/main/java/com/example/personal_dictionary_app/helpers/DatabaseHelper.java
+++ app/src/main/java/com/example/personal_dictionary_app/helpers/DatabaseHelper.java
@@ -30,6 +30,15 @@ public class DatabaseHelper {
     public static void addDummydata(){
         Date date = new Date();
         Word word = new Word("Effect", date.toString(), "","" ,"");
+        Word word2 = new Word("Triple", date.toString(), "Numbering","Triple Digit" ,"The count of three");
+        Word word3 = new Word("Service", date.toString(), "",
+                "The soldier are doing service to their country but fighting in wars" ,
+                "the action of helping or doing work for someone.");
+        Word word4 = new Word("Overview", date.toString(), "Viewpoint",
+                "This is the overview of the design" ,"a general review or summary of a subject");
         wordBank.add(word);
+        wordBank.add(word2);
+        wordBank.add(word3);
+        wordBank.add(word4);
     }
 }
