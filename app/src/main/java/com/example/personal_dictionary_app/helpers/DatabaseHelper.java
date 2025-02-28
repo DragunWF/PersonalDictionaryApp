c92152fbdef63b74e9ff2e73f5a11b8a00b27363
DragunDev <plarisanmarc@gmail.com>
2025-02-28 20:24:05 +0800

    Update DatabaseHelper.java

--- app/src/main/java/com/example/personal_dictionary_app/helpers/DatabaseHelper.java
+++ app/src/main/java/com/example/personal_dictionary_app/helpers/DatabaseHelper.java
@@ -3,14 +3,24 @@ package com.example.personal_dictionary_app.helpers;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.example.personal_dictionary_app.data.Word;
+
 public class DatabaseHelper {
     private static final String FILE_KEY = "db";
 
     private static SharedPreferences sharedPref;
     private static SharedPreferences.Editor editor;
 
+    private static ModelBank<Word> wordBank;
+
     public static void initialize(Context context) {
         sharedPref = context.getSharedPreferences(FILE_KEY, Context.MODE_PRIVATE);
         editor = sharedPref.edit();
+
+        wordBank = new ModelBank<>(sharedPref, editor, "words", Word.class);
+    }
+
+    public static ModelBank<Word> getWordBank() {
+        return wordBank;
     }
 }
