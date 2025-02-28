b3134b006c32c0da17c365118b9d2a22da9e6476
Aysaaak <isaac.6severino@gmail.com>
2025-02-28 20:19:08 +0800

    Word class

---
+++ app/src/main/java/com/example/personal_dictionary_app/data/Word.java
@@ -0,0 +1,70 @@
+package com.example.personal_dictionary_app.data;
+
+import androidx.annotation.NonNull;
+
+import java.time.LocalDate;
+
+public class Word extends Model{
+
+    String word, definition, usage, category;
+    LocalDate date;
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "word='" + word + '\'' +
+                ", definition='" + definition + '\'' +
+                ", usage='" + usage + '\'' +
+                ", category='" + category + '\'' +
+                ", date=" + date +
+                ", id=" + id +
+                '}';
+    }
+    public Word(String word, LocalDate date, String category, String usage, String definition) {
+        this.word = word;
+        this.date = date;
+        this.category = category;
+        this.usage = usage;
+        this.definition = definition;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    public String getUsage() {
+        return usage;
+    }
+
+    public void setUsage(String usage) {
+        this.usage = usage;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
