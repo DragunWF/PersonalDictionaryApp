bb153625b6c41e0486e3d375ef1ec7cab996f685
DragunDev <plarisanmarc@gmail.com>
2025-02-28 20:49:41 +0800

    Set adapater to the recycler view layout

--- app/src/main/java/com/example/personal_dictionary_app/adapters/WordAdapter.java
+++ app/src/main/java/com/example/personal_dictionary_app/adapters/WordAdapter.java
@@ -4,12 +4,17 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.personal_dictionary_app.R;
 import com.example.personal_dictionary_app.data.Word;
+import com.example.personal_dictionary_app.helpers.DatabaseHelper;
+import com.example.personal_dictionary_app.helpers.ModelBank;
+import com.example.personal_dictionary_app.helpers.Utils;
+import com.example.personal_dictionary_app.services.WordService;
 
 import java.util.List;
 
@@ -23,29 +28,38 @@ public class WordAdapter extends RecyclerView.Adapter<WordAdapter.ViewHolder> {
      * (custom ViewHolder)
      */
     public static class ViewHolder extends RecyclerView.ViewHolder {
-//        private final TextView wordText;
-//        private final TextView dateAddedText;
-//        private final TextView categoryText;
+        private final TextView wordText, dateAddedText, categoryText;
+        private final Button viewBtn, deleteBtn;
 
         public ViewHolder(View view) {
             super(view);
             // Define click listener for the ViewHolder's View
-//            wordText = view.findViewById(R.id.textView);
-//            dataAddedText = view.findViewById(R.id.);
-//            categoryText = view.findViewById(R.id.);
+            wordText = view.findViewById(R.id.wordText);
+            dateAddedText = view.findViewById(R.id.dateAddedText);
+            categoryText = view.findViewById(R.id.categoryText);
+            viewBtn = view.findViewById(R.id.viewBtn);
+            deleteBtn = view.findViewById(R.id.deleteBtn);
         }
 
-//        public TextView getWordText() {
-//            return wordText;
-//        }
-//
-//        public TextView getDateAddedText() {
-//            return dateAddedText;
-//        }
-//
-//        public TextView getCategoryText() {
-//            return categoryText;
-//        }
+        public TextView getWordText() {
+            return wordText;
+        }
+
+        public TextView getDateAddedText() {
+            return dateAddedText;
+        }
+
+        public TextView getCategoryText() {
+            return categoryText;
+        }
+
+        public Button getViewBtn() {
+            return viewBtn;
+        }
+
+        public Button getDeleteBtn() {
+            return deleteBtn;
+        }
     }
 
     /**
@@ -63,11 +77,10 @@ public class WordAdapter extends RecyclerView.Adapter<WordAdapter.ViewHolder> {
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         // Create a new view, which defines the UI of the list item
-//        View view = LayoutInflater.from(viewGroup.getContext())
-//                .inflate(R.layout.text_row_item, viewGroup, false);
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.recycle_view_card, viewGroup, false);
 
-//        return new ViewHolder(view);
-        return null;
+        return new ViewHolder(view);
     }
 
     // Replace the contents of a view (invoked by the layout manager)
@@ -76,7 +89,28 @@ public class WordAdapter extends RecyclerView.Adapter<WordAdapter.ViewHolder> {
 
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
-        // viewHolder.getTextView().setText(localDataSet[position]);
+//         viewHolder.getTextView().setText(localDataSet[position]);
+        Word word = localDataSet.get(position);
+        viewHolder.getWordText().setText(word.getWord());
+        viewHolder.getDateAddedText().setText(word.getDate().toString());
+        viewHolder.getCategoryText().setText(word.getCategory());
+        viewHolder.getViewBtn().setOnClickListener(v -> {
+
+        });
+        viewHolder.getDeleteBtn().setOnClickListener(v -> {
+            WordService.deleteWord(word.getId());
+            Utils.toast(word.getWord() + " has been deleted!", context);
+            updateData();
+        });
+    }
+
+    public void updateData() {
+        ModelBank<Word> wordBank = DatabaseHelper.getWordBank();
+        localDataSet.clear();
+        for (Word word : wordBank.getAll()) {
+            localDataSet.add(word);
+        }
+        notifyDataSetChanged();
     }
 
     // Return the size of your dataset (invoked by the layout manager)
